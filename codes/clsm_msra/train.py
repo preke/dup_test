af5235c28ef1626b7fc2e7c9f75fc83d3c3365eb
preke <geek00021@gmail.com>
2018-04-02 00:35:34 +0800

    .

--- codes/clsm_msra/train.py
+++ codes/clsm_msra/train.py
@@ -9,7 +9,7 @@ def train(train_iter, vali_iter, model, args):
     if args.cuda:
         model.cuda()
     parameters = list(filter(lambda p: p.requires_grad, model.parameters()))    
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(parameters, lr=args.lr)
     steps = 0
     best_acc = 0
     last_step = 0
