1e836c7a24b24fa95e70c2c7ee22a75412deb2f1
preke <geek00021@gmail.com>
2018-04-09 11:59:57 +0800

    .

--- codes/clsm_msra/model.py
+++ codes/clsm_msra/model.py
@@ -24,10 +24,8 @@ class CNN_clsm(nn.Module):
 
         self.embedding.weight.requires_grad = False
 
-        self.conv    = nn.Conv2d(in_channels=Ci, 
-                                 out_channels=Co, 
-                                 (K, D),
-                                 stride=1)
+        self.conv    = nn.Conv2d(in_channels=Ci, out_channels=Co, 
+                                 kernel_size=(K, D), stride=1)
         self.dropout = nn.Dropout(args.dropout, self.training)
         self.fc      = nn.Linear(in_features=Co, out_features=Ss)
 
