46210660b8969610c2d62daaf78b6b43d313c32a
preke <geek00021@gmail.com>
2018-04-09 10:47:21 +0800

    .

--- codes/clsm_msra/main.py
+++ codes/clsm_msra/main.py
@@ -124,7 +124,7 @@ test_data = data.TabularDataset(path=Test_path,
 # label_field.build_vocab(test_data)
 test_iter = data.Iterator(
     test_data,
-    batch_size=1,
+    batch_size=len(test_data),
     device=0,
     repeat=False)
 
